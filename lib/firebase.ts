dc0e6a91cf3abdcefd9fa2f3d3243842515bb27c
Nicolai <nicolai.bernhard03@gmail.com>
2022-06-08 21:32:32 +0200

    d

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,1 +1,12 @@
 import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAH4GZIvNFGv_2LIVgVm3FrGdukKtr9yG8",
+  authDomain: "whatever-for-now.firebaseapp.com",
+  projectId: "whatever-for-now",
+  storageBucket: "whatever-for-now.appspot.com",
+  messagingSenderId: "446798856933",
+  appId: "1:446798856933:web:c05f43c72517fb44138a0c",
+};
+
+const app = initializeApp(firebaseConfig);
